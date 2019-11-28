7ebae43a83aadae1717a5b755a1ff5af62a2578f
__Miracle__ <31105601+miraclestatus@users.noreply.github.com>
2019-11-28 10:04:17 +0800

    增加了前台页面的登录/注册/退出
    增加了前台页面的登录/注册/退出

--- 02-movie-project-login-out/app/home/views.py
+++ 02-movie-project-login-out/app/home/views.py
@@ -5,8 +5,19 @@
 # @File    : views.py
 # @Software: PyCharm
 from . import home
-from flask import render_template
+from flask import render_template, redirect, url_for
+
+
 @home.route("/")
 def index():
     # return "<h1 style='color:red'>home</h1>"
     return render_template('home/index.html')
+@home.route("/login/")
+def login():
+    return render_template('home/login.html')
+@home.route("/logout/")
+def logout():
+    return redirect(url_for('home.login'))
+@home.route("/regist/")
+def regist():
+    return render_template('home/register.html')
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # python_flask_movie_project
 使用Python的Flask迅速构建一个电影网站
+# Pycharm 常用的快捷键
+- reformat :code ctrl+alt+L
+- 文件的切换 ：ctrl + tab
