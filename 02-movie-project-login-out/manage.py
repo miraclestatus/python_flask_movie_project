b311cafde864f119f875161579c609cdcd9445ff
__Miracle__ <31105601+miraclestatus@users.noreply.github.com>
2019-12-12 10:19:15 +0800

    会员中心模块完善
    会员中心模块完善

--- 02-movie-project-login-out/manage.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2019/11/26 11:26
-# @Author  : Eric Lee
-# @Email   : li.yan_li@neusoft.com
-# @File    : manage.py
-# @Software: PyCharm
-from app import app
-if __name__ == '__main__':
-    app.run()
--- 03-movie-project-vip-center-search-404/app/home/views.py
+++ 03-movie-project-vip-center-search-404/app/home/views.py
@@ -24,3 +24,15 @@ def regist():
 @home.route("/user/")
 def user():
     return render_template("home/user.html")
+@home.route("/pwd/")
+def pwd():
+    return render_template("home/pwd.html")
+@home.route("/comments/")
+def comments():
+    return render_template("home/comments.html")
+@home.route("/loginlog/")
+def loginlog():
+    return render_template("home/loginlog.html")
+@home.route("/moviecol/")
+def moviecol():
+    return render_template("home/moviecol.html")
